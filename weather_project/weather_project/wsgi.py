4e92f8763cd6387365fd8daab30d512ca38d4155
ptran9102 <hoangphuctran9102@gmail.com>
2023-08-17 23:01:43 -0400

    Deploying to Railway

--- weather_project/weather_project/wsgi.py
+++ weather_project/weather_project/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_app.settings')
+sys.path.append('/home/django_projects/weather_project')
+sys.path.append('/home/django_projects/weather_project/weather_project')
 
 application = get_wsgi_application()
